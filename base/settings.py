b3a5f4d125985b65feca91dced0f2ceec35883b7
jooeunho <pureredgrace@gmail.com>
2023-07-18 00:21:18 +0900

    setting env

--- base/settings.py
+++ base/settings.py
@@ -11,21 +11,34 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
+# dajango-env : https://django-environ.readthedocs.io/en/latest/
+env = environ.Env()
+
+env_path = BASE_DIR / ".env"
+if env_path.exists():
+    with env_path.open("rt", encoding="utf8") as f:
+        env.read_env(f)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-51+fbdaty94j2ih2-ls^m)ta%5_x++#qq2)&lh7!f7g@iet1he'
+
+SECRET_KEY = env.str(
+    "SECRET_KEY",
+    default='django-insecure-51+fbdaty94j2ih2-ls^m)ta%5_x++#qq2)&lh7!f7g@iet1he'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=True)  # 1,t, true
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
 
 # Application definition
@@ -79,10 +92,7 @@ WSGI_APPLICATION = 'base.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db("DATABASE_URL", default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
 }
 
 
@@ -111,7 +121,7 @@ AUTH_USER_MODEL = "accounts.User"
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'UTC'
 
@@ -124,6 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = env.str('STATIC_ROOT', default=BASE_DIR / 'staticfiles')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = env.str('MEDIA_ROOT', default=BASE_DIR / 'mediafiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -132,6 +147,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # django-debug-toolbar
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
+INTERNAL_IPS = env.list("INTERNAL_IPS", default=['127.0.0.1'])

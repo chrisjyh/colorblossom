6084192e59a339145d2bcb669a356e7e105f427e
jooeunho <pureredgrace@gmail.com>
2023-11-01 16:33:16 +0900

    create survey model

--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,29 @@
+import logging
+
 from django.db import models
+from typing import List
+
+logger = logging.getLogger(__name__)
 
 # Create your models here.
+class Survey(models.Model):
+    uid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=500, unique=True)
+    reg_date = models.DateTimeField(auto_now_add=True)
+    use_yn = models.CharField(max_length=1, default='y')
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-reg_date']
+        db_table = 'survey'
+    
+class Question(models.Model):
+    pass
+
+class Choice(models.Model):
+    pass
+
+class Result(models.Model):
+    pass

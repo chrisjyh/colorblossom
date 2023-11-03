442a1e99f93f214b91f8d92373a02aae8106b513
jooeunho <pureredgrace@gmail.com>
2023-11-03 20:36:19 +0900

    survey edit

--- survey/models.py
+++ survey/models.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.db import models
+from accounts.models import User
 from typing import List
 
 logger = logging.getLogger(__name__)
@@ -20,10 +21,42 @@ class Survey(models.Model):
         db_table = 'survey'
     
 class Question(models.Model):
-    pass
+    uid = models.AutoField(primary_key=True)
+    survey = models.ForeignKey(
+        Survey,
+        on_delete=models.CASCADE
+    )
+    question = models.TextField()
+    amount_choice = models.TextField(default=0)
+    use_yn = models.CharField(max_length=1, default='y')
+    
+    def __str__(self):
+        return self.question
 
 class Choice(models.Model):
-    pass
+    uid = models.AutoField(primary_key=True)
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE
+    )
+    choice = models.TextField()
+    score = models.IntegerField(default=0)
 
 class Result(models.Model):
-    pass
+    uid = models.AutoField(primary_key=True)
+    survey = models.ForeignKey(
+        Survey,
+        on_delete=models.CASCADE
+    )
+    type = models.CharField(max_length=500)
+    explane = models.TextField()
+    
+class collect_result(models.Model):
+    uid = models.AutoField(primary_key=True)
+    survey = models.ForeignKey(
+        Survey,
+        on_delete=models.CASCADE
+    )
+    line_result = models.CharField(max_length= 500)
+    body_result = models.CharField(max_length= 500)
+    reg_date = models.DateTimeField(auto_now_add=True)

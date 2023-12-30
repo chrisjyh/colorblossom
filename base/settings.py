c4eb723d8dedc36df7e8b0d28e20070cb14d19d7
jooeunho <pureredgrace@gmail.com>
2023-12-31 02:08:01 +0900

    front edt

--- base/settings.py
+++ base/settings.py
@@ -13,8 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import environ
 
-import pymysql
-pymysql.install_as_MySQLdb()
+# import pymysql
+# pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- base/urls.py
+++ base/urls.py
@@ -10,10 +10,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("accounts.urls")),
     path('faq/', include("faq.urls")),
+    path('aboutus',TemplateView.as_view(template_name="aboutUs.html"), name="aboutUs"),
+    path('program/basic',TemplateView.as_view(template_name="program/basicExamine.html"), name="basicExam"),
+    path('program/body',TemplateView.as_view(template_name="program/bodyExamine.html"), name="bodyExam"),
+    path('program/pro',TemplateView.as_view(template_name="program/consultExamine.html"), name="consultExam"),
+    path('program/simple',TemplateView.as_view(template_name="program/simpleExamine.html"), name="simpleExam"),
+    path('program/pro',TemplateView.as_view(template_name="program/proExamine.html"), name="proExam"),
     path('selftest/', TemplateView.as_view(template_name="selftest/selftest.html"), name="selftest"),
     path("", views.index, name="main")
 ]
-
 
 if settings.DEBUG:
     urlpatterns += [
